e3929207c46ba2dcd6ba106f398eed859c63699e
Denis Yaremov <denis.yaremov@gmail.com>
2019-08-25 18:23:15 +0300

    Minor cleaning

--- bin/launch.js
+++ bin/launch.js
@@ -1,13 +1,6 @@
 #!/usr/bin/env node
+
 const path = require('path');
-const { Runner } = require('.');
+const { Runner } = require('../build');
 
 Runner.main(path.join(__dirname, '..'));
-
-// const fs = require('fs');
-// const path = require('path');
-
-// const lnk = fs.readlinkSync(process.argv[1]);
-// const cwd = path.join(path.dirname(process.argv[1]), path.dirname(lnk), '..');
-
-// require(path.join(cwd, 'bootstrap.js')).perform(cwd);
